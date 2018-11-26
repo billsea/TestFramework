dca82111ca9d5036f40effab4b88ee4e543524d5
Bill S <bill.e.seaman@gmail.com>
2018-11-25 22:07:25 -0800

    initial commit

---
+++ TestFramework/TestModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestModel.swift
+//  TestFramework
+//
+//  Created by Loud on 11/20/18.
+//  Copyright © 2018 William Seaman. All rights reserved.
+//
+
+import Foundation
+
+public struct testModel {
+  public var modelValue: String!
+}
